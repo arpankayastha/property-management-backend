ec753581301826ee1c8bbe425b1d9e6a2e949dd6
foram lalani <114900428+ForamLalani@users.noreply.github.com>
2022-11-29 11:18:04 +0530

    Auto fill data from ical data

--- src/controllers/PropertiesController.ts
+++ src/controllers/PropertiesController.ts
@@ -1,6 +1,7 @@
 import {BodyParams, Controller, Delete, Get, PathParams, Post, QueryParams} from "@tsed/common";
 import {Property} from "../entities/Property";
 import {PropertiesRepository} from "../repositories/PropertiesRepository";
+const ical = require('node-ical');
 
 @Controller("/properties")
 export class PropertiesController {
@@ -78,7 +79,57 @@ export class PropertiesController {
             };
         }
     }
+    @Post("/autoFillData")
+    async autoFillData(@BodyParams() bodyParams: any): Promise<object> {
+        try {
+            
+            const properties =await this.propertiesRepository.findOne({ id: bodyParams.id });
+            console.log(properties?.propertyLockFile);
+            const events = await ical.async.fromURL(properties?.propertyLockFile);
+            let eventsList: any = Object.values(events);
+            let summaryList     = [];
+            
+           
+            for (const event of eventsList) {
+                if (event && event.type == "VEVENT") {
 
+                    let summaryObj = event.summary.split('; ');
+                    let customerName =  summaryObj[0].split('  (');
+                   let otherInfo = '';
+                  
+                    if(typeof summaryObj[5] != 'undefined') {
+                         otherInfo = summaryObj[5];
+                    }
+                  
+                    var log = {
+                    startDate : event.start.toISOString(),
+                    endDate : event.end.toISOString(),
+                    customerName : customerName[0],
+                    email:summaryObj[3],
+                    roomNamae:summaryObj[2],
+                    contactNumber:summaryObj[4],
+                    otherInfo:otherInfo
+                    }
+                    summaryList.push(log);
+                }
+            }
+
+            return {
+                success: true,
+                code   : 200,
+                message: 'Auto fill data has been retrieved.',
+                data   : {
+                    icalData: summaryList
+                }
+            };
+        } catch (error) {
+            return {
+                success: false,
+                code   : 201,
+                message: error.message || 'Something went wrong. Please contact support team.',
+            };
+        }
+    }
     @Delete("/:id")
     async delete(@PathParams('id') id: string): Promise<object | undefined> {
         try {
